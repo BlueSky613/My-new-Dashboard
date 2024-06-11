09868c400bcb899f62de682d2155ac2dda19306a
Tiger <stylistdire68@gmail.com>
2024-06-11 18:40:27 +1000

    611-3

--- src/pages/ScrapingPage.js
+++ src/pages/ScrapingPage.js
@@ -30,7 +30,7 @@ export default function ScrapingPage() {
     },
   ];
 
-//git
+//gitorial
   return (
     <div className="pt-20 text-black scraping-page-background h-[85%]">
       <div className="ml-36 flex flex-col gap-8 mt-20 font-bold text-2xl">
